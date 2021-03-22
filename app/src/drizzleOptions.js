f180badc90b9a76b2bcdca3c23c31a10ec649b0c
asooge <asooge@gmail.com>
2021-03-21 21:11:55 -0400

    fix metamask

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,10 +1,16 @@
 import Web3 from 'web3';
 import PleaseFundMe from './contracts/PleaseFundMe.json';
 
+window.ethereum.enable();
+
 const options = {
   web3: {
     block: false,
-    customProvider: new Web3('ws://localhost:8545'),
+    customProvider: new Web3(window.ethereum),
+    fallBack: {
+      type: 'ws',
+      url: 'ws://localhost:8545',
+    },
   },
   contracts: [PleaseFundMe],
   events: {},
