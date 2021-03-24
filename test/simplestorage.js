6f5c04a0193e16ae6c71f6297da2de11f3ce4a1b
asooge <asooge@gmail.com>
2021-03-24 19:32:15 -0400

    update test files to pass for deployment

--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,14 +1,14 @@
-const SimpleStorage = artifacts.require('SimpleStorage');
+const PleaseFundMe = artifacts.require('PleaseFundMe');
 
-contract('SimpleStorage', (accounts) => {
+contract('PleaseFundMe', (accounts) => {
   it('...should store the value 89.', async () => {
-    const simpleStorageInstance = await SimpleStorage.deployed();
+    const pleaseFundMeInstance = await PleaseFundMe.deployed();
 
     // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+    await pleaseFundMeInstance.set(89, { from: accounts[0] });
 
     // Get stored value
-    const storedData = await simpleStorageInstance.storedData.call();
+    const storedData = await pleaseFundMeInstance.storedData.call();
 
     assert.equal(storedData, 89, 'The value 89 was not stored.');
   });
