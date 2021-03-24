d7d4f1df3ea599742aec7da24daa9b50c490b249
asooge <34760728+asooge@users.noreply.github.com>
2021-03-24 19:39:01 -0400

    update test files to pass for deployment (#21)

--- test/TestSimpleStorage.sol
+++ test/TestSimpleStorage.sol
@@ -1,17 +1,17 @@
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity >=0.7.0 <0.8.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/SimpleStorage.sol";
+import "../contracts/PleaseFundMe.sol";
 
-contract TestSimpleStorage {
+contract TestPleaseFundMe {
     function testItStoresAValue() public {
-        SimpleStorage simpleStorage = SimpleStorage(DeployedAddresses.SimpleStorage());
+        PleaseFundMe pleaseFundMe = PleaseFundMe(DeployedAddresses.PleaseFundMe());
 
-        simpleStorage.set(89);
+        pleaseFundMe.set(89);
 
         uint expected = 89;
 
-        Assert.equal(simpleStorage.storedData(), expected, "It should store the value 89.");
+        Assert.equal(pleaseFundMe.storedData(), expected, "It should store the value 89.");
     }
 }
